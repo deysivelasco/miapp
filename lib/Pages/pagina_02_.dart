4194b4770dd8a8a5934a002f4085466db5023a63
deysivelasco <irma_deysi@hotmail.com>
2021-11-24 17:50:10 -0600

    pagina 02 constryendo

---
+++ lib/Pages/pagina_02_.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Pagina02 extends StatelessWidget {
+  const Pagina02({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pagina 02"),
+      ),
+    );
+  }
+}
