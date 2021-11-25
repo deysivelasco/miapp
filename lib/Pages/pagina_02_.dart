a8a9b8e3ec114935b359e4b0e5fb6f796109f94f
deysivelasco <irma_deysi@hotmail.com>
2021-11-25 15:52:40 -0600

    se anexo boton para regresar

--- lib/Pages/pagina_02_.dart
+++ lib/Pages/pagina_02_.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class Pagina02 extends StatelessWidget {
@@ -7,8 +9,50 @@ class Pagina02 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Pagina 02"),
+        title: const Text("Términos y Condiciones"),
       ),
+    body:Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Text("Términos y condiciones", 
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold ),),
+          SizedBox(height: 15,),
+          Text("Para usar esta aplicación es necesario que aceptes los términos y condiciones.",
+          style: TextStyle(fontSize: 16),textAlign: TextAlign.justify,),
+          SizedBox(height: 15,),
+          Text("Para usar esta aplicación es necesario que aceptes los términos y condiciones.",
+          style: TextStyle(fontSize: 16),textAlign: TextAlign.justify,),
+          SizedBox(height: 15,),
+                Text("Para usar esta aplicación es necesario que aceptes los términos y condiciones. ",
+          style: TextStyle(fontSize: 16),textAlign: TextAlign.justify,),
+          SizedBox(height: 15,),
+          RaisedButton(
+            padding: EdgeInsets.all(15),
+            color: Colors.blue,
+            textColor: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Text("Acepto todo", style: TextStyle(fontSize:
+                20 ),),
+                Icon(Icons.arrow_forward_ios)
+              ],
+            ),
+            onPressed: ()=>{
+              //Aqui por ejemplo podrían guardar un registro de este celular en una base de datos en firebase.
+
+              Navigator.pop(context)
+          
+
+        
+            })
+          
+
+        ],
+      ),
+    ) ,
     );
   }
 }
